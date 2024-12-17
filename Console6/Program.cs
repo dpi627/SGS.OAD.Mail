2c601f8456f4cd80c9a4cb3c0ac0946a5b002df1
brian <dpi.studio@gmail.com>
2024-12-17 22:56:54 +0800

    測試設定檔讀取

--- Console6/Program.cs
+++ Console6/Program.cs
@@ -6,16 +6,16 @@ namespace Console6
     {
         static void Main(string[] args)
         {
-            Email.Create()
+            var mail = Email.Create()
                 //.UseSmtp("smtp-apac.sgs.net", 25, false)
                 //.WithCredentials("username", "password")
                 //.From("brian_li@sgs.com") //預設 no-reply@sgs.com
                 .To("brian_li@sgs.com") //"brian_li@sgs.com", "amy.hu@sgs.com"
-                //.Cc("brian.li.sgs@gmail.com")
+                                        //.Cc("brian.li.sgs@gmail.com")
                 .Subject("測試郵件")
-                .Body("<h1>Hello World!</h1>", isHtml: true)
+                .Body("<h1>Hello World!</h1>", isHtml: true);
                 //.Attach("/path/to/attachment.pdf")
-                .Send();
+                mail.Send();
         }
     }
 }
