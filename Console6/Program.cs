24ee5c407f2c91f561376fe0566ca501cbb058b6
brian_li <brian.li@sgs.com>
2024-12-17 16:50:37 +0800

    完成基礎測試範例

--- Console6/Program.cs
+++ Console6/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Console6
+﻿using SGS.OAD.Mail;
+
+namespace Console6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Email.Create()
+                //.UseSmtp("smtp-apac.sgs.net", 25, false)
+                //.WithCredentials("username", "password")
+                //.From("brian_li@sgs.com") //預設 no-reply@sgs.com
+                .To("brian_li@sgs.com") //"brian_li@sgs.com", "amy.hu@sgs.com"
+                //.Cc("brian.li.sgs@gmail.com")
+                .Subject("測試郵件")
+                .Body("<h1>Hello World!</h1>", isHtml: true)
+                //.Attach("/path/to/attachment.pdf")
+                .Send();
         }
     }
 }
