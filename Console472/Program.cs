6d40795ae48f1c8eccdf0080f125b879dce400ab
brian_li <brian.li@sgs.com>
2024-12-18 14:16:50 +0800

    測試專案撰寫測試範例

--- Console472/Program.cs
+++ Console472/Program.cs
@@ -1,14 +1,27 @@
 ﻿using SGS.OAD.Mail;
+using System.Collections.Generic;
 
 namespace Console472
 {
     internal class Program
     {
-        static void Main()
+        static async void Main()
         {
             Email.Create()
-                .To("brian.li@sgs.com")
-                .Send();
+                .To("someone@sgs.com")
+                .SendWithRetry();
+
+            await Email.Create()
+                .To("someone@sgs.com")
+                .SendAsyncWithRetry();
+
+            var list = new List<Email>
+            {
+                Email.Create().To("user1@sgs.com"),
+                Email.Create().To("user2@sgs.com")
+            };
+            await Email.SendMultipleAsync(list);
+            await Email.SendMultipleWithRetryAsync(list);
         }
     }
 }
